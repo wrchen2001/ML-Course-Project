4471bbad81d02b1679b969b4577e7187bf7c047b
Weiran Chen <130285745+wrchen2001@users.noreply.github.com>
2023-12-15 10:38:02 +0800

    Update Function.py

--- LSTM/Function.py
+++ LSTM/Function.py
@@ -1,12 +1,8 @@
-# coding=UTF-8
-
 import torch
 import random
 import numpy as np
 import pandas as pd
- 
-from Error_calculation import MAE, MSE, NRMSE, RMSE
- 
+
 
 # 设置随机数种子，保证每次运行结果相同
 def setup_seed(seed):
@@ -58,20 +54,3 @@ def sliding_window(normalized_data, train_length, val_length, time_step, forecas
  
     return [train_input, train_output, val_input, val_output, test_input, test_output]
  
-
-# 计算预测值和目标值之间的误差
-def cmpt_error(predicted, target):
-    contrast = pd.DataFrame(np.hstack((predicted, target)), columns=['预测值', '目标值'])
-    print(contrast)
-
-    mae = MAE(predicted, target)
-    mse = MSE(predicted, target)
-    rmse = RMSE(predicted, target)
-    nrmse = NRMSE(predicted, target)
- 
-    print('MAE误差: ', mae)
-    print('MSE误差: ', mse)
-    print('RMSE误差: ', rmse)
-    print(f'NRMSE误差: {"%.4f" % (nrmse * 100)}%')
- 
-    return [mae, mse, rmse, nrmse]
